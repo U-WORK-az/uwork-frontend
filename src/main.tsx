aa39a56bc61978879d1275e199173c257c7ad540
gahramanova <narminhikmat@gmail.com>
2025-06-03 15:42:08 +0200

    Installed bootstrap, materialUi, and scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "./assets/sass/style.scss"
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
