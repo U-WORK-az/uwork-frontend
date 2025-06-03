2319e7d3a3e80e799374798e0b8c06f962a8b530
gahramanova <narminhikmat@gmail.com>
2025-06-03 15:28:15 +0200

    Created pages, installing react-router-dom and prepare lazy and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+
+const Home = lazy(() => import("./pages/Home"))
+const About = lazy(() => import("./pages/About"))
+const Header = lazy(() => import("./layout/Header"))
+const Footer = lazy(() => import("./layout/Footer"))
+const Profile = lazy(() => import("./pages/Profile"))
+const Notifications = lazy(() => import("./pages/Notifications"))
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/notifications" element={<Notifications />} />
+        </Routes>
+
+      </Suspense>
+      <Footer />
+    </>
   )
 }
 
