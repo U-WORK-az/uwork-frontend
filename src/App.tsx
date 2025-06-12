2f8c8be4ec8dfb2dcff29ccc6c28fa7a91c6465a
MinayeAliyeva <7s346pu@code.edu.az>
2025-06-12 14:17:55 +0400

    feat:structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/home/index.tsx"));
+const About = lazy(() => import("./pages/about/index.tsx"));
+const Notifications = lazy(() => import("./pages/notifications/index.tsx"));
 const MainLayout = lazy(() => import("./layout/MainLayout"));
+const ProfileLayout = lazy(() => import("./layout/ProfileLayout"));
 const ProfilePage = lazy(() => import("./pages/profile/index"));
+
 const App = () => (
   <Routes>
-    <Route path="/*" element={<MainLayout />} />
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/notifications" element={<Notifications />} />
+    </Route>
 
-    <Route path="/profile" element={<ProfilePage />} />
+    <Route path="profile" element={<ProfileLayout />}>
+      <Route index element={<ProfilePage />} />
+    </Route>
   </Routes>
 );
 
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,21 +1,13 @@
 import { lazy } from "react";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Header = lazy(() => import("../layout/header/index.tsx"));
 const Footer = lazy(() => import("../layout/footer/index.tsx"));
-const Home = lazy(() => import("../pages/home/index.tsx"));
-const About = lazy(() => import("../pages/about/index.tsx"));
-const Notifications = lazy(() => import("../pages/notifications/index.tsx"));
 
 const MainLayout = () => (
   <>
     <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/notifications" element={<Notifications />} />
-    </Routes>
-    <Outlet/>
+    <Outlet />
     <Footer />
   </>
 );
